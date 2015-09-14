6e7b7b0433a71ea0aaf04b638ee3a70fa4ac05e5
wanghan <wh85125@163.com>
2015-09-14 17:30:32 +0800

    add iOS file #1

--- frontEnd/here_ios/here_ios/AppDelegate.h
+++ frontEnd/here_ios/here_ios/AppDelegate.h
@@ -8,7 +8,8 @@
 
 #import <UIKit/UIKit.h>
 #import "UserInfoModel.h"
-#import "socket.IO-objc/SocketIO.h"
+#import "SocketIO.h"
+//#import "socket.IO-objc/SocketIO.h"
 #import "TWTSideMenuViewController.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, SocketIODelegate>
