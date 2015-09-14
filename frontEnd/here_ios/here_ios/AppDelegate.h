4641f4f255a2ffecdebf3e7e3768e4b359940550
wanghan <wh85125@163.com>
2015-09-14 15:19:38 +0800

    add iOS init file

---
+++ frontEnd/here_ios/here_ios/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  here_ios
+//
+//  Created by wang jam on 9/14/15.
+//  Copyright (c) 2015 jam wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
