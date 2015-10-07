f0d5f571ada7f6121d4bdf55e2365f179c4aa165
wanghan <wh85125@163.com>
2015-10-08 00:00:36 +0800

    change

--- backEnd/here_dev/sh_script/stopall.sh
+++ backEnd/here_dev/sh_script/stopall.sh
@@ -1,4 +1,4 @@
 
 
-forever stop $HOME/dev/here/backEnd/here_dev/server.js
-forever stop $HOME/dev/here/backEnd/here_dev/socket.js
+forever stop $HOME/here/backEnd/here_dev/server.js
+forever stop $HOME/here/backEnd/here_dev/socket.js
