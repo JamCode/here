9c85829e2a565d64be7c240170830c0be626ae8e
wanghan <wh85125@163.com>
2015-10-13 14:34:26 +0800

    #81

--- backEnd/here_dev/sh_script/errorLogReport.sh
+++ backEnd/here_dev/sh_script/errorLogReport.sh
@@ -3,4 +3,6 @@
 grep -i -n error $HOME/logs/*>$HOME/err_report.txt
 
 #if error exist, send email to monitor
+mydate=`date +"%Y%m%d"`
 
+node $HOME/here/backEnd/here_dev/utility/sendEmail.js "错误报告-"mydate "no"
