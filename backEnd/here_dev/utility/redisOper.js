d82c99d0c1372174b6e0ed9071e405f52e621441
wanghan <wh85125@163.com>
2015-10-25 22:54:05 +0800

    #99

---
+++ backEnd/here_dev/utility/redisOper.js
@@ -0,0 +1,48 @@
+var redis = require('redis');
+var redis_client = redis.createClient();
+var config = require('../config/config');
+var log = global.log;
+
+
+
+exports.increaseUnreadGoodCount = function(user_id) {
+	//input unread good count to redis
+	redis_client.hget(config.goodUnreadCount, user_id, function(err, reply) {
+
+		log.debug('increase unread good count for '+user_id);
+
+		if (err) {
+			log.error(err, log.getFileNameAndLineNum(__filename));
+			return;
+		}
+
+		if (reply === null) {
+			reply = 1;
+		} else {
+			reply = parseInt(reply) + 1;
+		}
+		redis_client.hset(config.goodUnreadCount, user_id, reply);
+	});
+}
+
+exports.increaseUnreadCommentCount = function(user_id) {
+	//input unread good count to redis
+	redis_client.hget(config.commentUnreadCount, user_id, function(err, reply) {
+
+		log.debug('increase unread comment count for '+user_id);
+
+		if (err) {
+			log.error(err, log.getFileNameAndLineNum(__filename));
+			return;
+		}
+
+		if (reply === null) {
+			reply = 1;
+		} else {
+			reply = parseInt(reply) + 1;
+		}
+		
+		redis_client.hset(config.commentUnreadCount, user_id, reply);
+	});
+}
+
