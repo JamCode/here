eb24bdc4a6410dc97aaeb5424fcd50532b4cb954
yuzhongzhu <yuzhongzhu123@gmail.com>
2015-11-02 23:09:49 +0800

    Revert "#111 table designs for static pv and uv"
    This reverts commit a8bf18ff2da4abfab9b79c3ca9202a27c4628955.

--- backEnd/here_dev/sql/1.0.7.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS  `static_pv_uv`;
-CREATE TABLE `static_pv_uv` (
-  `static_id` varchar(100) NOT NULL,
-  `pv_count` int(11) NOT NULL,
-  `uv_count` int(11) NOT NULL,
-  `static_timestamp` timestamp  NOT NULL DEFAULT current_timestamp ,
-  `static_date` timestamp NOT NULL,
-  PRIMARY KEY (`static_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
