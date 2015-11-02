341c834deb5226f417317ac4fc0bc9458b697bf5
yuzhongzhu <yuzhongzhu123@gmail.com>
2015-11-02 23:55:58 +0800

    modify column dataType

--- backEnd/here_dev/sql/1.0.7.sql
+++ backEnd/here_dev/sql/1.0.7.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS  `daliy_report`;
 CREATE TABLE `daliy_report` (
   `pv_count` int(11) NOT NULL,
   `uv_count` int(11) NOT NULL,
-  `static_timestamp` varchar(20)  NOT NULL,
-  `static_date` varchar(20) NOT NULL
+  `timestamp` bigint  NOT NULL,
+  `date` varchar(20)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
