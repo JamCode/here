f0cdb409b27181d6224b3e3e6f2ec1bfc20143f6
wanghan <wh85125@163.com>
2015-11-07 19:49:17 +0800

    #131

---
+++ backEnd/here_dev/sh_script/sysReport.pl
@@ -0,0 +1,25 @@
+#! /usr/bin/perl
+
+
+#using for get system status report
+
+#crontab task
+
+#send email to monitor
+
+$HOME = $ENV{HOME};
+$env = $ENV{ENV};
+
+`df -h>$HOME/sys_report.txt`;
+
+my $time = shift || time();
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime($time);
+$year += 1900;
+$mon ++;
+
+$subject = $year."-".$mon."-".$mday."-文件系统报告_".$env;
+print $subject."\n";
+
+$filePath = "$HOME/sys_report.txt";
+
+`node $HOME/here/backEnd/here_dev/utility/sendEmail.js $subject $filePath &`
