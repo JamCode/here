20c93866589055fc48cf81cb05b324136e58f944
wanghan <wh85125@163.com>
2015-11-18 17:55:43 +0800

    #150

--- backEnd/here_dev/routes/routeFunc.js
+++ backEnd/here_dev/routes/routeFunc.js
@@ -2,19 +2,19 @@ var log = global.log;
 var config = require('../config/config');
 
 
-exports.feedBack = function(flag, result, res, req) {
+exports.feedBack = function(flag, result, res, sq) {
 
 	var returnData = {};
 	if (flag) {
 		returnData.code = config.returnCode.SUCCESS;
 		returnData.data = result;
 	} else {
-		log.error(result, log.getFileNameAndLineNum(__filename), req.body.sq);
+		log.error(result, log.getFileNameAndLineNum(__filename), sq);
 		returnData.code = config.returnCode.ERROR;
 		//email.sendMail(result);
 
 	}
 	log.debug(JSON.stringify(returnData), log.getFileNameAndLineNum(__filename),
-		req.body.sq);
+		sq);
 	res.send(returnData);
 };
