a0163cedca62aaf46c57a3b4f1d32961fd18fef9
wanghan <wh85125@163.com>
2015-11-19 18:08:43 +0800

    #147

--- backEnd/here_dev/routes/contentRouter.js
+++ backEnd/here_dev/routes/contentRouter.js
@@ -37,7 +37,7 @@ router.post('/commentGood', function(req, res) {
 
 			//推送到前端
 			apnToUser(req.body.comment_user_id, req.body.user_name + '赞了你的评论');
-
+			redisOper.increaseUnreadCommentGoodCount(req.body.comment_user_id);
 
 		}else{
 			log.debug(result.code, log.getFileNameAndLineNum(__filename), req.body.sq);
