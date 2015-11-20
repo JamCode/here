07c7c5e624424a58632593971159c40313223e77
wanghan <wh85125@163.com>
2015-11-20 14:11:40 +0800

    #156

--- backEnd/here_dev/sh_script/pvUvRprt.pl
+++ backEnd/here_dev/sh_script/pvUvRprt.pl
@@ -10,15 +10,15 @@ $HOME = $ENV{HOME};
 #Run and Create Rport data task
 system("`node $HOME/here/backEnd/here_dev/utility/staticRprt.js &`");
 
-my $time = shift || time();
-my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime($time);
-$year += 1900;
-$mon ++;
+# my $time = shift || time();
+# my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime($time);
+# $year += 1900;
+# $mon ++;
+#
+# my $mydate= $year."-".$mon."-".$mday;
+#
+# printf $mydate;
 
-my $mydate= $year."-".$mon."-".$mday;
-
-printf $mydate;
-
-#system("`tar czvf $HOME/log_back/$mydate\'_accessLog.tar\' $HOME/here/backEnd/here_dev/access.log &`");
+system("`tar czvf $HOME/log_back/accessLog.tar $HOME/here/backEnd/here_dev/access.log`");
 
 system("`>$HOME/here/backEnd/here_dev/access.log`");
