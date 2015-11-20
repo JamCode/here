5775b69d4e0f84c454c95a10d7e675d7005087bb
wanghan <wh85125@163.com>
2015-11-20 17:47:32 +0800

    #156

--- backEnd/here_dev/sh_script/pvUvRprt.pl
+++ backEnd/here_dev/sh_script/pvUvRprt.pl
@@ -7,23 +7,21 @@
 
 $HOME = $ENV{HOME};
 
-my $time = shift || time();
-my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime($time);
-$year += 1900;
-$mon ++;
 
-my $mydate= $year."-".$mon."-".$mday;
-
-printf $mydate;
-
-system("tar czvf $HOME/log_back/access_log.tar $HOME/here/backEnd/here_dev/access.log");
 
 
 #Run and Create Rport data task
 print "count pv and upv\n";
 system("node $HOME/here/backEnd/here_dev/utility/staticRprt.js");
 
+my $time = shift || time();
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime($time);
+$year += 1900;
+$mon ++;
+my $mydate= $year."-".$mon."-".$mday;
+printf $mydate;
 
+`tar czvf $HOME/log_back/access_log.tar $HOME/here/backEnd/here_dev/access.log`;
 
 
 print "clear access.log\n";
