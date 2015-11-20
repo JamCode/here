7cb930a925d540a7eddd2d6b663416b33d96f11b
wanghan <wh85125@163.com>
2015-11-20 14:05:39 +0800

    #156

--- backEnd/here_dev/sh_script/pvUvRprt.pl
+++ backEnd/here_dev/sh_script/pvUvRprt.pl
@@ -21,4 +21,4 @@ printf $mydate;
 
 system("`tar czvf $HOME/log_back/$mydate\'_accessLog.tar\' $HOME/here/backEnd/here_dev/access.log &`");
 
-system("`rm -rf $HOME/here/backEnd/here_dev/access.log &`");
+system("`>$HOME/here/backEnd/here_dev/access.log`");
