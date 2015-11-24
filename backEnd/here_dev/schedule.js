3189d1237f392e2a351c78a2650ecf0ca59a3a96
wanghan <wh85125@163.com>
2015-11-24 23:30:12 +0800

    #156

--- backEnd/here_dev/schedule.js
+++ backEnd/here_dev/schedule.js
@@ -1,9 +1,17 @@
 var log = require('./utility/log');
 log.SetLogFileName('logSchedule_');
 global.log = log; // 设置全局
+var fs = require('fs');
 
 var schedule = require("node-schedule");
 var pvCountRprt = require("./utility/staticRprt.js");
+var path = require('path');
+
+
+var pidfile = path.join(process.env.HOME, '/schedule.pid');
+fs.writeFileSync(pidfile, process.pid, {
+    flag: 'w'
+});
 
 
 schedule.scheduleJob('57 23 * * *', function(){
