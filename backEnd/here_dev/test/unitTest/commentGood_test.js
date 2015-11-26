3b9bfe05fc15d15b6e0080b4a21d865c6810303a
wanghan <wh85125@163.com>
2015-11-26 10:32:43 +0800

    #147

--- backEnd/here_dev/test/unitTest/commentGood_test.js
+++ backEnd/here_dev/test/unitTest/commentGood_test.js
@@ -1,12 +1,3 @@
-var http = require('http');
-var global_config;
-if (process.env.ENV === 'dev') {
-    global_config = require('../../config/dev_env_config');
-}
-if (process.env.ENV === 'pro') {
-    global_config = require('../../config/pro_env_config');
-}
-var hostname = '112.74.102.178';
 
 
 var commentGoodJson = {
@@ -17,31 +8,5 @@ var commentGoodJson = {
     user_name: 'commentGoodtest'
 };
 
-var options = {
-    port: global_config.httpServerInfo.listen_port,
-    hostname: hostname,
-    method: 'POST',
-    path: commentGoodJson.childpath,
-    headers: {
-        'Content-Type': 'application/json; encoding=utf-8',
-        'Accept': 'application/json',
-        'Content-Length': JSON.stringify(commentGoodJson).length
-    }
-};
-
-var body = '';
-
-var req = http.request(options, function(res) {
-    console.log("Got response: " + res.statusCode);
-    res.on('data', function(d) {
-        body += d;
-    }).on('end', function() {
-        console.log(res.headers);
-        console.log(body);
-    });
-}).on('error', function(e) {
-    console.log("Got error: " + e.message);
-});
-
-req.write(JSON.stringify(commentGoodJson)+"\n");
-req.end();
+var runner = require('unitTestRunner.js');
+runner.runTest(commentGoodJson, commentGoodJson.childpath);
