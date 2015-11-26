039b117cbabaaa201c977a9d4e0147a3218d10df
wanghan <wh85125@163.com>
2015-11-26 17:20:00 +0800

    Revert "#161"
    This reverts commit 25ad40fc1b86a63d0c34e37f633e5f06ba17f358.

--- frontEnd/here_ios/here_ios/View/ContentTableViewCell.m
+++ frontEnd/here_ios/here_ios/View/ContentTableViewCell.m
@@ -135,9 +135,6 @@ static const int ageWidth = 18;
         _distanceLabel.font = [UIFont fontWithName:@"Arial" size:timeFontSize];
         _distanceLabel.textColor = [UIColor lightGrayColor];
         
-        _deleteLabel = [[UILabel alloc] init];
-        
-        
         [self addSubview:_faceView];
         [self addSubview:_nickName];
         
@@ -150,7 +147,6 @@ static const int ageWidth = 18;
         [self addSubview:_goodCountLabel];
         [self addSubview:_commentCountLabel];
         [self addSubview:_distanceLabel];
-        [self addSubview:_deleteLabel];
         
         imageArray = [[NSMutableArray alloc] init];
         
@@ -499,17 +495,6 @@ static const int ageWidth = 18;
     
     _distanceLabel.text = [[NSString alloc] initWithFormat:@"%@", [Tools showDistance:userLocation otherLocation:myLocation]];
     [Tools resizeLabel:_distanceLabel maxHeight:contentDetailInfoHeight maxWidth:100 fontSize:timeFontSize];
-    
-    
-    
-    
-    UserInfoModel* myinfo = [AppDelegate getMyUserInfo];
-    
-    if([model.userInfo.userID isEqualToString:myinfo.userID]){
-        
-    }else{
-        
-    }
     
 }
 
