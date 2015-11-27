a856fac7d760cfa08cd61bd01ae5a0383f94a6f4
wanghan <wh85125@163.com>
2015-11-27 15:12:00 +0800

    #111

--- backEnd/here_dev/schedule.js
+++ backEnd/here_dev/schedule.js
@@ -6,6 +6,7 @@ var fs = require('fs');
 var schedule = require("node-schedule");
 var pvCountRprt = require("./utility/staticRprt.js");
 var path = require('path');
+var email = require('./utility/emailTool');
 
 
 var pidfile = path.join(process.env.HOME, '/schedule.pid');
@@ -19,4 +20,13 @@ log.info("run schedule", log.getFileNameAndLineNum(__filename));
 schedule.scheduleJob('57 23 * * *', function(){
     log.info("pv count start", log.getFileNameAndLineNum(__filename));
     pvCountRprt.start();
+});
+
+
+
+process.on('uncaughtException', function(err) {
+    log.error('schedule process Caught exception: ' +
+        err.stack);
+    email.sendMail('schedule process Caught exception: ' + err.stack,
+        'schedule process failed');
 });
