7fb572adcc423ad4c1967e7bb249485cdc793fcd
wanghan <wh85125@163.com>
2015-12-22 16:33:45 +0800

    #188

--- backEnd/here_dev/schedule.js
+++ backEnd/here_dev/schedule.js
@@ -2,7 +2,7 @@ var log = require('./utility/log');
 log.SetLogFileName('logSchedule_');
 global.log = log; // 设置全局
 var fs = require('fs');
-
+require('shelljs/global');
 var schedule = require("node-schedule");
 var pvCountRprt = require("./utility/staticRprt.js");
 var path = require('path');
@@ -17,11 +17,20 @@ fs.writeFileSync(pidfile, process.pid, {
 
 log.info("run schedule", log.getFileNameAndLineNum(__filename));
 
+
+
+//统计pv和upv
 schedule.scheduleJob('57 23 * * *', function(){
     log.info("pv count start", log.getFileNameAndLineNum(__filename));
     pvCountRprt.start();
 });
 
+
+//文件系统报告
+schedule.scheduleJob('58 23 * * *', function(){
+    log.info("file system report", log.getFileNameAndLineNum(__filename));
+    //exec()
+});
 
 
 process.on('uncaughtException', function(err) {
