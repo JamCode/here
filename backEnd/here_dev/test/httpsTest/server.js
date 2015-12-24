8e46b64b78a9d9a8fc5a96a6035806459df69397
wanghan <wh85125@163.com>
2015-12-24 16:31:25 +0800

    #193

--- backEnd/here_dev/test/httpsTest/server.js
+++ backEnd/here_dev/test/httpsTest/server.js
@@ -1,14 +1,21 @@
-var httpsModule = require('https');
-var fs = require('fs');
-var port = 4433;
-var https = httpsModule.Server({
-     key: fs.readFileSync('./server.key'),
-     cert: fs.readFileSync('./server.crt')
-}, function(req, res){
-    res.writeHead(200);
-    res.end("hello world\n");
-});
+// var httpsModule = require('https');
+// var fs = require('fs');
+// var port = 4433;
+// var https = httpsModule.Server({
+//      key: fs.readFileSync('./server.key'),
+//      cert: fs.readFileSync('./server.crt')
+// }, function(req, res){
+//     res.writeHead(200);
+//     res.end("hello world\n");
+// });
+//
+// https.listen(port, function(err){
+//      console.log("https listening on port: "+port);
+// });
 
-https.listen(port, function(err){
-     console.log("https listening on port: "+port);
+var http = require('http');
+var port = 4433;
+var server = http.createServer(function(req, res){
+    res.end('hello world\n');
 });
+server.listen(port);
