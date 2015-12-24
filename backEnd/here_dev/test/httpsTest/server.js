4458455722e68b38a01914e8ae79301d733c8860
wanghan <wh85125@163.com>
2015-12-24 16:38:13 +0800

    #193

--- backEnd/here_dev/test/httpsTest/server.js
+++ backEnd/here_dev/test/httpsTest/server.js
@@ -13,9 +13,27 @@
 //      console.log("https listening on port: "+port);
 // });
 
+
+var https = require('https');
+var fs = require('fs');
+var port1 = 4433;
+
+var options = {
+  key: fs.readFileSync('./server.key'),
+  cert: fs.readFileSync('./server.crt')
+};
+
+https.createServer(options, function (req, res) {
+  res.writeHead(200);
+  res.end("hello world\n");
+}).listen(port1);
+
+
+
+
 var http = require('http');
-var port = 4433;
+var port2 = 4434;
 var server = http.createServer(function(req, res){
     res.end('hello world\n');
 });
-server.listen(port);
+server.listen(port2);
