37e83c37b92f002c83bc136063ad132d3d562e7e
JamCode <wh85125@163.com>
2016-01-18 20:48:39 +0800

    #205

--- backEnd/here_dev/routes/webRouter.js
+++ backEnd/here_dev/routes/webRouter.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var router = express.Router();
-
+var config = require('../config/config');
 
 
 //
@@ -19,6 +19,14 @@ router.get('/login', function(req, res){
 router.post('/login', function(req, res){
 	console.log(req.body.name);
 	console.log(req.body.password);
+	if(config.mgmtUserInfo.name === req.body.name&&config.mgmtUserInfo.password === req.body.password){
+		console.log('validate successful');
+		req.flash('success', '登入成功');
+	}else{
+		console.log('validate not successful');
+		req.flash('error', '用户或密码错误');
+		req.redirect('/login');
+	}
 });
 
 
