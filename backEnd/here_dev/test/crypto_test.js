1dfe5e920a6f81870ebc8df165e720fee6ed2d65
wanghan <wh85125@163.com>
2016-03-24 17:37:42 +0800

    #236

--- backEnd/here_dev/test/crypto_test.js
+++ backEnd/here_dev/test/crypto_test.js
@@ -2,16 +2,17 @@ var crypto = require('crypto');
 
 var password = crypto.createHash('sha384').update('madaxiao', 'utf8').digest();
 console.log(password);
-var key = new Buffer(32);
-var iv = new Buffer(16);
-password.copy(key, 0, 0, 32);
-password.copy(iv, 0, 32, 16);
-console.log(key);
-console.log(iv);
-console.log(password);
+var key = new Buffer('280f8bb8c43d532f389ef0e2a5321220b0782b065205dcdfcb8d8f02ed5115b9');
+var iv = new Buffer('CC0A69779E15780ADAE46C45EB451A23');
+// password.copy(key, 0, 0, 32);
+// password.copy(iv, 0, 32, 16);
+// console.log(key);
+// console.log(iv);
+// console.log(password);
 
-var decipher = crypto.createDecipheriv('aes128', key, iv);
-var encrypted = 'Wj6zUKv3us9kwKaaYR+8Bg==';
+var decipher = crypto.createDecipheriv('aes256', key,
+iv);
+var encrypted = 'WQYg5qvcGyCBY3IF0hPsoQ==';
 var decrypted = decipher.update(encrypted, 'base64', 'utf8');
 decrypted += decipher.final('utf8');
 console.log(decrypted);
