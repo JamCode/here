9b2cb092224364086fa5f0b16fe439ac1326e716
王翰 <wanghan@cfets.zh>
2018-04-07 20:17:27 +0800

    add all

--- frontEnd/here_ios/Pods/SDWebImage/SDWebImage/SDWebImageDownloaderOperation.m
+++ frontEnd/here_ios/Pods/SDWebImage/SDWebImage/SDWebImageDownloaderOperation.m
@@ -211,9 +211,6 @@ NSString *const SDWebImageDownloadFinishNotification = @"SDWebImageDownloadFinis
 
 #pragma mark NSURLConnection (delegate)
 
-
-
-
 - (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
     
     //'304 Not Modified' is an exceptional one
@@ -445,24 +442,25 @@ NSString *const SDWebImageDownloadFinishNotification = @"SDWebImageDownloadFinis
 }
 
 - (void)connection:(NSURLConnection *)connection willSendRequestForAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge{
-//    if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]) {
-//        if (!(self.options & SDWebImageDownloaderAllowInvalidSSLCertificates) &&
-//            [challenge.sender respondsToSelector:@selector(performDefaultHandlingForAuthenticationChallenge:)]) {
-//            [challenge.sender performDefaultHandlingForAuthenticationChallenge:challenge];
-//        } else {
-//            NSURLCredential *credential = [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust];
-//            [[challenge sender] useCredential:credential forAuthenticationChallenge:challenge];
-//        }
-//    } else {
+    if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]) {
+        if (!(self.options & SDWebImageDownloaderAllowInvalidSSLCertificates) &&
+            [challenge.sender respondsToSelector:@selector(performDefaultHandlingForAuthenticationChallenge:)]) {
+            [challenge.sender performDefaultHandlingForAuthenticationChallenge:challenge];
+        } else {
+            NSURLCredential *credential = [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust];
+            [[challenge sender] useCredential:credential forAuthenticationChallenge:challenge];
+        }
+    } else {
         if ([challenge previousFailureCount] == 0) {
-            //NSURLCredential 这个类是表示身份验证凭据不可变对象。凭证的实际类型声明的类的构造函数来确定。
-            NSURLCredential* cre = [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust];
-            [challenge.sender useCredential:cre forAuthenticationChallenge:challenge];
-            
+            if (self.credential) {
+                [[challenge sender] useCredential:self.credential forAuthenticationChallenge:challenge];
+            } else {
+                [[challenge sender] continueWithoutCredentialForAuthenticationChallenge:challenge];
+            }
         } else {
             [[challenge sender] continueWithoutCredentialForAuthenticationChallenge:challenge];
         }
-    //}
+    }
 }
 
 @end
